ea83f63cb41c2b3be84e9ebbfb08d7c64dbe12be
funilrys <contact@funilrys.com>
2018-04-28 14:13:31 +0200

    Fix the way we show the output when converting a file.

--- domain2idna/__init__.py
+++ domain2idna/__init__.py
@@ -91,9 +91,9 @@ def file(file_to_convert, output=None):
         converted = Core(to_convert).to_idna()
 
         if output:
-            File(output).write('\n'.join(converted), overwrite=True)
+            File(output).write("\n".join(converted), overwrite=True)
         else:
-            print(converted)
+            print("\n".join(converted))
 
 
 def command():
