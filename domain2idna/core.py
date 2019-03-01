19db140bf88b0007c0f199244082376b8aaa5f40
funilrys <contact@funilrys.com>
2019-03-01 19:00:25 +0100

    Update of the license year

--- domain2idna/core.py
+++ domain2idna/core.py
@@ -21,7 +21,7 @@ Project documentation:
 License:
     MIT License
 
-    Copyright (c) 2018 Nissar Chababy
+    Copyright (c) 2018-2019 Nissar Chababy
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
