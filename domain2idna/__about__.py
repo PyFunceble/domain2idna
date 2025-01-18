6129945a18f7a60c2f7ddb9c4f2f42dd225a0841
funilrys <contact@funilrys.com>
2025-01-18 16:17:59 +0100

    Update LICENSE.

--- domain2idna/__about__.py
+++ domain2idna/__about__.py
@@ -19,8 +19,8 @@ License:
 
     MIT License
 
-    Copyright (c) 2018, 2019, 2020, 2021, 2022, 2023, 2024 Nissar Chababy
-    Copyright (c) 2019, 2020, 2021, 2022, 2023, 2024 PyFunceble Contributors
+    Copyright (c) 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025 Nissar Chababy
+    Copyright (c) 2019, 2020, 2021, 2022, 2023, 2024, 2025 PyFunceble Contributors
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
@@ -41,4 +41,4 @@ License:
     SOFTWARE.
 """
 
-__version__ = "1.12.2"
+__version__ = "1.12.3"
